c032f3afda2942167a287b5b9b8ef31430cab8da
Jashpatel1 <patel.5@iitj.ac.in>
2020-03-23 18:01:38 +0530

    added block styles

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Block from './Block';
 
 class Blocks extends Component {
     state = { blocks: [] };
@@ -17,8 +18,8 @@ class Blocks extends Component {
                 {
                     this.state.blocks.map(block => {
                         return (
-                            <div key={block.hash}> {block.hash}</div>
-                        )
+                            <Block key={block.hash} block={block} />
+                            )
                     })
                 }
             </div>
