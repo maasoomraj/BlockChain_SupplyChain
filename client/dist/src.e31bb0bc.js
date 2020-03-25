91e27abc9c3e155402eb89f5d77746dff267fbb1
maasoomraj <raj.8@iitj.ac.in>
2020-03-25 21:25:15 +0530

    added

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -44050,7 +44050,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50877" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51405" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
