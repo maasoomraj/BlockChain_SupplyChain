8368a1f8592df6d346b39c13c02455e737fc5a44
maasoomraj <raj.8@iitj.ac.in>
2020-05-22 20:40:14 +0530

    removed Home

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,7 +6,6 @@ class Navigation extends Component {
         return (
             <div className='navbar'>
                     <Link to ='/' className ="heading">SUPPLY CHAIN MANAGEMENT</Link>
-                    <Link to ='/'>Home</Link>
                     <Link to='/blocks'>Blocks</Link>
                     <Link to='/conduct-transaction'>Conduct a Transaction</Link>
                     <Link to='/send-transaction'>Send</Link>
