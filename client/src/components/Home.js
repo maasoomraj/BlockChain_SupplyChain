8c332e71d3babb1fcd126416db5e35ba3fc30b90
Jashpatel1 <patel.5@iitj.ac.in>
2020-08-01 15:28:09 +0530

    Configure Production3

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -40,7 +40,7 @@ class Home extends Component {
                 </Link>
 
                 <Button bsstyle="danger" bssize="small">
-                    <a href='client/src/components/files/MyWallet.txt' target="_blank" download='MyWallet.txt'>Click to download</a>
+                    <a href='src/components/files/MyWallet.txt' target="_blank" download='MyWallet.txt'>Click to download</a>
                 </Button>
                 
                 
